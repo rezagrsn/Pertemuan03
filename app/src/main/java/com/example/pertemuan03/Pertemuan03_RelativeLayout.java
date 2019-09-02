b32ec00bc21e756cbb862d6488f47bcd60648d0f
rezagrsn <reza.reynaldi@ti.ukdw.ac.id>
2019-09-02 19:43:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pertemuan03/Pertemuan03_RelativeLayout.java
@@ -0,0 +1,14 @@
+package com.example.pertemuan03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pertemuan03_RelativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pertemuan03__relative_layout);
+    }
+}
---
+++ app/src/main/java/com/example/pertemuan03/pertemuan03_ConstraintLayout.java
@@ -0,0 +1,14 @@
+package com.example.pertemuan03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pertemuan03_ConstraintLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pertemuan03__constraint_layout);
+    }
+}
