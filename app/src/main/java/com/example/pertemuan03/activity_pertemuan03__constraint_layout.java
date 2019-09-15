f7fdfe78080500c38d288ccba75f2687dc837aea
rezagrsn <reza.reynaldi@ti.ukdw.ac.id>
2019-09-16 00:00:02 +0700

    update tugas

--- app/src/main/java/com/example/pertemuan03/activity_pertemuan03__constraint_layout.java
+++ app/src/main/java/com/example/pertemuan03/activity_pertemuan03__constraint_layout.java
@@ -1,6 +1,9 @@
 package com.example.pertemuan03;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -28,5 +31,37 @@ public class activity_pertemuan03__constraint_layout extends AppCompatActivity {
         Intent intent = new Intent(this, activity_pertemuan03__relative_layout.class);
         startActivity(intent);
 
+    }
+
+    public void loadFragmentOne(View v) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        Fragment fragment1 = fragmentManager.findFragmentByTag("fragOne");
+
+        if (fragment1 == null) {
+            fragment1 = new FragmentOne();
+        }
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.mainFrag, fragment1, "fragOne");
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    public void loadFragmentTwo(View v) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        Fragment fragment2 = fragmentManager.findFragmentByTag("fragTwo");
+
+        if (fragment2 == null) {
+            fragment2 = new FragmentTwo();
+        }
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.mainFrag, fragment2, "fragTwo");
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/pertemuan03/fragment_simple_2.java
@@ -0,0 +1,33 @@
+package com.example.pertemuan03;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class fragment_simple_2 extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_simple_2,container,false);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+}
