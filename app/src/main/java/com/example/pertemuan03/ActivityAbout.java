f80e8bade915a65608179c99e63caf8e657f1493
rezagrsn <reza.reynaldi@ti.ukdw.ac.id>
2019-10-28 02:22:35 +0700

    update tugas SharedPreferences & List Mahasiswa

---
+++ app/src/main/java/com/example/pertemuan03/ActivityAbout.java
@@ -0,0 +1,60 @@
+package com.example.pertemuan03;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+
+public class ActivityAbout extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> listData;
+    private Button btnDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_activity);
+
+        listData = new ArrayList<>();
+        setData();
+
+        btnDatabase = (Button) findViewById(R.id.btnDatabase);
+        btnDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityDatabase();
+            }
+        });
+
+        recyclerView = findViewById(R.id.recycleView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new Recycle_Adapter(listData);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setData(){
+        listData.add("Data Satu");
+        listData.add("Data Dua");
+        listData.add("Data Tiga");
+        listData.add("Data Empat");
+        listData.add("Data Lima");
+    }
+
+    public void openActivityDatabase(){
+        Intent intent = new Intent(this, ActivityDatabase.class);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/com/example/pertemuan03/RecycleViewDb.java
@@ -0,0 +1,63 @@
+package com.example.pertemuan03;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecycleViewDb extends RecyclerView.Adapter<RecycleViewDb.ViewHolder> {
+    private Context context;
+    private Cursor cursor;
+
+    public RecycleViewDb(Context context, Cursor cursor) {
+        this.context = context;
+        this.cursor = cursor;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup vg , int viewType){
+        View v= LayoutInflater.from(context).inflate(R.layout.daftar_mahasiswa,vg,false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder vh, int pos){
+        if (!cursor.moveToPosition(pos)) {
+            return;
+        }
+        String nim = cursor.getString(cursor.getColumnIndex(DataMahasiswa.mahasiswaEntry.COLUMN_NIM));
+        String nama = cursor.getString(cursor.getColumnIndex(DataMahasiswa.mahasiswaEntry.COLUMN_NAMA));
+        String noHP = cursor.getString(cursor.getColumnIndex(DataMahasiswa.mahasiswaEntry.COLUMN_NOHP));
+        vh.nim.setText(nim);
+        vh.nama.setText(nama);
+        vh.noHP.setText(noHP);
+    }
+    @Override
+    public int getItemCount(){
+        return cursor.getCount();
+    }
+    public void swapCursor(Cursor newCur){
+        if(cursor !=null){
+            cursor.close();
+        }
+        cursor = newCur;
+        if(newCur!=null){
+            notifyDataSetChanged();
+        }
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView nim;
+        public TextView nama;
+        public TextView noHP;
+        public ViewHolder(View view) {
+            super(view);
+            nim = (TextView) view.findViewById(R.id.col1);
+            nama = (TextView) view.findViewById(R.id.col2);
+            noHP = (TextView) view.findViewById(R.id.col3);
+        }
+    }
+}
